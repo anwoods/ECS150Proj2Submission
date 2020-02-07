d2d39e5c27f31d1ca85e97a58bca30eb088fce9e
anwoods <35635573+anwoods@users.noreply.github.com>
2020-02-06 17:39:40 -0800

    fix queue returns

--- progs/queue_tester.c
+++ progs/queue_tester.c
@@ -37,13 +37,128 @@ void test_queue_simple(void)
 	TEST_ASSERT(ptr == &data);
 }
 
-void test_queue_destroy_full(void){
+void test_queue_destroy_not_empty(void){
     queue_t q;
     int data1 = 3;
-    fprintf(stderr, "*** TEST queue_destroy_full ***\n");
+    fprintf(stderr, "*** TEST queue_destroy_not_empty ***\n");
     q = queue_create();
     queue_enqueue(q, &data1);
     int res = queue_destroy(q);
+    TEST_ASSERT(res == -1);
+}
+
+void test_queue_destroy_empty(void){
+    queue_t q;
+    fprintf(stderr, "*** TEST queue_destroy_empty ***\n");
+    q = queue_create();
+    int res = queue_destroy(q);
+    q = NULL;
+    TEST_ASSERT(q == NULL);
+    TEST_ASSERT(res == 0);
+}
+
+void test_queue_destroy_null(void){
+    queue_t q;
+    fprintf(stderr, "*** TEST queue_destroy_null ***\n");
+    q = queue_create();
+    q = NULL;
+    int res = queue_destroy(q);
+    TEST_ASSERT(res == -1);
+}
+
+void test_enqueue_null_queue(void){
+	queue_t q;
+	int data = 3;
+    fprintf(stderr, "*** TEST enqueue_null_queue ***\n");
+    q = queue_create();
+    q = NULL;
+    int res = queue_enqueue(q, &data);
+    TEST_ASSERT(res == -1);
+}
+
+void test_enqueue_null_data(void){
+	queue_t q;
+    fprintf(stderr, "*** TEST enqueue_null_data ***\n");
+    q = queue_create();
+    int res = queue_enqueue(q, NULL);
+    TEST_ASSERT(res == -1);
+}
+
+void test_enqueue_success(void){
+    queue_t q;
+    fprintf(stderr, "*** TEST enqueue_success ***\n");
+    q = queue_create();
+    int data = 3;
+    int res = queue_enqueue(q, &data);
+    TEST_ASSERT(res == 0);
+    TEST_ASSERT(queue_length(q) == 1);
+}
+
+void test_dequeue_null_queue(void){
+	queue_t q;
+	int *ptr;
+    fprintf(stderr, "*** TEST dequeue_null_queue ***\n");
+    q = NULL;
+    int res = queue_dequeue(q, (void**)&ptr);
+    TEST_ASSERT(res == -1);
+}
+
+void test_dequeue_null_data(void){
+	queue_t q;
+	int *ptr = NULL;
+	int data = 3;
+    fprintf(stderr, "*** TEST dequeue_null_data ***\n");
+    q = queue_create();
+    queue_enqueue(q, &data);
+    int res = queue_dequeue(q, (void**)&ptr);
+    TEST_ASSERT(res == -1);
+}
+
+void test_dequeue_empty_queue(void){
+	queue_t q;
+	int *ptr;
+    fprintf(stderr, "*** TEST dequeue_empty_queue ***\n");
+    q = queue_create();
+    int res = queue_dequeue(q, (void**)&ptr);
+    TEST_ASSERT(res == -1);
+}
+
+void test_delete_null_queue(void){
+	queue_t q;
+	int data = 3;
+    fprintf(stderr, "*** TEST delete_null_queue ***\n");
+    q = NULL;
+    int res = queue_delete(q, &data);
+    TEST_ASSERT(res == -1);
+}
+
+void test_delete_null_data(void){
+	queue_t q;
+	int data = 3;
+    fprintf(stderr, "*** TEST delete_null_data ***\n");
+    q = queue_create();
+    queue_enqueue(q, &data);
+    int res = queue_delete(q, NULL);
+    TEST_ASSERT(res == -1);
+}
+
+void test_delete_data_not_found(void){
+	queue_t q;
+	int data = 3;
+	int not_in_queue_data = 4;
+    fprintf(stderr, "*** TEST delete_data_not_found ***\n");
+    q = queue_create();
+    queue_enqueue(q, &data);
+    int res = queue_delete(q, &not_in_queue_data);
+    TEST_ASSERT(res == -1);
+}
+
+void test_delete_empty_queue(void){
+	queue_t q;
+	int not_in_queue_data = 4;
+    fprintf(stderr, "*** TEST delete_empty_queue ***\n");
+    q = queue_create();
+    int res = queue_delete(q, &not_in_queue_data);
     TEST_ASSERT(res == -1);
 }
 
@@ -51,7 +166,19 @@ int main(void)
 {
 	test_create();
 	test_queue_simple();
-	test_queue_destroy_full();
+	test_queue_destroy_not_empty();
+	test_queue_destroy_empty();
+	test_queue_destroy_null();
+	test_enqueue_null_queue();
+	test_enqueue_null_data();
+	test_enqueue_success();
+	test_dequeue_null_queue();
+	test_dequeue_null_data();
+	test_dequeue_empty_queue();
+	test_delete_null_queue();
+	test_delete_null_data();
+	test_delete_data_not_found();
+	test_delete_empty_queue();
 
 	return 0;
 }
