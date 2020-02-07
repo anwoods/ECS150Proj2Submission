8abd255afddb315a00e4a1a7ebe9ebd59058a98c
anwoods <35635573+anwoods@users.noreply.github.com>
2020-02-06 23:34:38 -0800

    got rid off stuff

--- progs/test_preempt.c
+++ progs/test_preempt.c
@@ -12,12 +12,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "uthread.h"
+#include <uthread.h>
 
 int thread3(void* arg)
 {
 	while(1){
-		printf("--------------------------thread3 = %d %s\n", uthread_self(), (char*)arg);
+		printf("thread3 = %d\n", uthread_self());
 	}
 	return 0;
 }
@@ -26,7 +26,7 @@ int thread2(void* arg)
 {
 	uthread_create(thread3, NULL);
 	uthread_yield();
-	printf("--------------------------thread2 = %d %s\n", uthread_self(), (char*)arg);
+	printf("thread2 = %d\n", uthread_self());
 	return 0;
 }
 
@@ -34,7 +34,7 @@ int thread1(void* arg)
 {
 	uthread_create(thread2, NULL);
 	uthread_yield();
-	printf("--------------------------thread1 = %d %s\n", uthread_self(), (char *)arg);
+	printf("thread1 = %d\n", uthread_self());
 	return 0;
 }
 
